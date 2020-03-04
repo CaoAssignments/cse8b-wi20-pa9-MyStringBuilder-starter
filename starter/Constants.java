b59125b920a75981ca2b227b3d3b814d8d128cef
dheeraj7596 <dheeraj7596.dm@gmail.com>
2020-03-04 00:52:04 -0800

    assignment added

---
+++ starter/Constants.java
@@ -0,0 +1,12 @@
+public class Constants {
+    public static final String FROM_APPEND_STR = "append(String)";
+    public static final String FROM_INSERT_CHAR = "insert(char, int)";
+    public static final String FROM_INSERT_STR = "insert(String, int)";
+    public static final String FROM_FIND_INDEX = "findIndex(int)";
+    public static final String FROM_REMOVE = "remove(int)";
+    public static final String FROM_DELETE_STARTINDEX = "delete(int)";
+    public static final String FROM_DELETE_STARTINDEX_ENDINDEX = "delete(int, int)";
+    public static final String FROM_SUBSTRING_STARTINDEX = "substring(int)";
+    public static final String FROM_SUBSTRING_STARTINDEX_ENDINDEX = "substring(int, int)";
+    public static final String FROM_CONCAT = "concat(MyStringBuilder)";
+}
